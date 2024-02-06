9c14d0de9021b054e51fa00cdaf74bc7ed80bba6
sandhyaganta <gantasandhyavitasoft@gmail.com>
2024-02-06 21:27:25 +0530

    mainpage completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",component:MainpageComponent},
+    {path:"mainpage",component:MainpageComponent}
+];
