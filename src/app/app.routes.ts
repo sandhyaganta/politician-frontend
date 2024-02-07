aa529b15309f2f1b3220b2000f890bbba04c3ab9
sandhyaganta <gantasandhyavitasoft@gmail.com>
2024-02-07 11:41:26 +0530

    complete the about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { MainpageComponent } from './mainpage/mainpage.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
     {path:"",component:MainpageComponent},
-    {path:"mainpage",component:MainpageComponent}
+    {path:"mainpage",component:MainpageComponent},
+    {path:"about",component:AboutComponent}
 ];
--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,9 +1,10 @@
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-mainpage',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.css'
 })
